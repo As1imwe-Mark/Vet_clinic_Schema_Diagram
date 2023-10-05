b287e27c19c1dab21d118f93016eea1fc3479bf1
Kehinde Aminat <aimenah@outlook.com>
2023-10-05 15:36:22 +0100

    Update schema_based_on_diagram.sql

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,53 +1,55 @@
 CREATE TABLE patients (
-  id INT GENERATED ALWAYS AS IDENTITY,
-  name VARCHAR(100),
-  date_of_birth DATE,
-  PRIMARY KEY(id)
-);
-
-CREATE TABLE treatments (
-  id INT GENERATED ALWAYS AS IDENTITY,
-  name VARCHAR(100),
-  type VARCHAR(255),
-  PRIMARY KEY(id)
+	patient_id INTEGER PRIMARY KEY,
+	name VARCHAR,
+	date_of_birth DATE	
 );
 
 CREATE TABLE medical_histories (
-  id INT GENERATED ALWAYS AS IDENTITY,
-  admitted_at TIMESTAMP,
-  status VARCHAR(100),
-  patient_id INT REFERENCES patients(id),
-  PRIMARY KEY(id)
+	id INTEGER PRIMARY KEY,
+	admitted_at TIMESTAMP,
+	patient_id INTEGER,
+	status VARCHAR,
+	FOREIGN KEY (patient_id) REFERENCES patients(patient_id)
+);
+
+CREATE TABLE treatments (
+	id INTEGER PRIMARY KEY,
+	type VARCHAR,
+	name VARCHAR
 );
 
 CREATE TABLE invoices (
-  id INT GENERATED ALWAYS AS IDENTITY,
-  total_amount DECIMAL,
-  generated_at TIMESTAMP,
-  paid_at TIMESTAMP,
-  medical_history_id INT REFERENCES medical_histories(id),
-  PRIMARY KEY(id)
+	id INTEGER PRIMARY KEY,
+	total_amount DECIMAL,
+	generated_at TIMESTAMP,
+	payed_at TIMESTAMP,
+	medical_history_id INTEGER,
+	FOREIGN KEY (medical_history_id) REFERENCES medical_histories (id)
 );
+
 
 CREATE TABLE invoice_items (
-  id INT GENERATED ALWAYS AS IDENTITY,
-  unit_price DECIMAL,
-  quantity INT,
-  total_price DECIMAL,
-  invoice_id INT REFERENCES invoices(id),
-  treatment_id INT REFERENCES treatments(id),
-  PRIMARY KEY(id)
+	id INTEGER PRIMARY KEY,	unit_price DECIMAL,
+	quantity INTEGER,
+	total_price DECIMAL,
+	invoice_id INTEGER,
+	treatment_id INTEGER,
+	FOREIGN KEY (invoice_id) REFERENCES invoices (id),
+	FOREIGN KEY (treatment_id) REFERENCES treatments (id)
 );
 
--- Create a join table for the many-to-many relationship between treatments and medical_histories
+
 CREATE TABLE medical_history_treatments (
-  medical_history_id INT,
-  treatment_id INT,
-  PRIMARY KEY(medical_history_id, treatment_id),
-  FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id),
-  FOREIGN KEY (treatment_id) REFERENCES treatments(id)
+    medical_history_id INTEGER,
+    treatment_id INTEGER,
+    PRIMARY KEY (medical_history_id, treatment_id),
+    FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id),
+    FOREIGN KEY (treatment_id) REFERENCES treatments(id)
 );
 
--- Add foreign key indexes for the join table
-CREATE INDEX idx_medical_history_id ON medical_history_treatments(medical_history_id);
-CREATE INDEX idx_treatment_id ON medical_history_treatments(treatment_id);
+CREATE INDEX idx_medical_history_id_invoices ON invoices(medical_history_id);
+CREATE INDEX idx_patient_id_medical_histories ON medical_histories(patient_id);
+CREATE INDEX idx_invoice_id_invoice_items ON invoice_items(invoice_id);
+CREATE INDEX idx_treatment_id_invoice_items ON invoice_items(treatment_id);
+CREATE INDEX idx_medical_history_id_treatments ON medical_history_treatments(medical_history_id);
+CREATE INDEX idx_treatment_id_treatments ON medical_history_treatments(treatment_id);
